06a2d8c8f0ec9dabdf37e92377ec5cb19eddd8bf
Alexa Gillman <alexagillman@microsoft.com>
2024-03-12 15:59:36 -0400

    Create a professional profile page for myself

--- app.ts
+++ app.ts
@@ -1,6 +1,14 @@
-console.log("hello world");
-console.log("nice to meet you!!");
-console.log("hello world");
-console.log("nice to meet you!!");
-console.log("hello world");
-console.log("nice to meet you!!");
+// Import a web framework, such as Express
+const express = require("express");
+// Create a server
+const app = express();
+// Serve static files from the profile directory
+app.use(express.static("profile"));
+// Add a route that responds with the profile page when requested
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/profile/index.html");
+});
+// Listen on a port, such as 3000
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
